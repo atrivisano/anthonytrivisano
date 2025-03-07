f6cb4a4ae9bb991dde6b0f8d619a7e60024c0be7
Anthony Trivisano <anthony.trivisano@gmail.com>
2025-03-07 13:51:54 -0500

    Update contact api

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
-
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  site: 'https://anthonytrivisano.com',
-  integrations: [mdx(), sitemap()],
+    output: 'server',
+    site: 'https://anthonytrivisano.com',
+    integrations: [mdx(), sitemap()],
 
-  vite: {
-      plugins: [tailwindcss()],
-      ssr: {
-          noExternal: ['@sendgrid/mail'],
-      },
-	},
+    vite: {
+        plugins: [tailwindcss()],
+        ssr: {
+            noExternal: ['@sendgrid/mail'],
+        },
+    },
 
-  adapter: netlify(),
+    adapter: netlify(),
 });
